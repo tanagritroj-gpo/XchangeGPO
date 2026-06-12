d998cedfea8eb73fe2973ee86dd279930e479c37
Tanagrit Rojgitjanurak <tanagritrojgitjanurak@MacBook-Air-khxngTanagrit.local>
2026-06-13 01:14:24 +0700

    new

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -17,8 +17,8 @@ export async function createClient() {
         set(name: string, value: string, options: any) {
           cookieStore.set(name, value, options);
         },
-        remove(name: string, options: any) {
-          cookieStore.delete(name, options);
+        remove(name: string) {
+          cookieStore.delete(name);
         },
       },
     }
